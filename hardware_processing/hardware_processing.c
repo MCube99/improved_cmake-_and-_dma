e7fb01bdf1f21cdde85082361fa92cb60208942c
Maaz Muhammad Madha <mmadha@trtest.com>
2026-09-18 21:54:54 +0100

    Got an m somehow

--- hardware_processing/hardware_processing.c
+++ hardware_processing/hardware_processing.c
@@ -73,9 +73,8 @@ static pio_spi_t pio_spi;
 // GLOBALS
 // -----------------------------------------------------------------------------
 
-PRIVATE void __time_critical_func(pio_spi_write8_read8_blocking)(const pio_collection_t *pio_collection,const uint8_t *src,uint8_t *dst, size_t len);
-PRIVATE uint8_t __time_critical_func(pio_spi_write8_blocking)(const pio_collection_t *pio_collection, const uint8_t src, uint32_t *dest, size_t len);
-PRIVATE uint read_register(const pio_collection_t *pio_collection, const enum pio_src_dest reg);
+PRIVATE void __time_critical_func(pio_spi_write8_blocking)(const pio_collection_t *pio_collection, const uint32_t src, uint32_t *dest);
+PRIVATE uint32_t read_register(const pio_collection_t *pio_collection, const enum pio_src_dest reg);
 //PRIVATE uint32_t ReadRxValue(PIO pio, uint sm);
 //PRIVATE uint32_t ReadTxValue(PIO pio, uint sm);
 // -----------------------------------------------------------------------------
@@ -343,13 +342,16 @@ PUBLIC bool usb_processing_main(void) {
     }
 }
 
+// -----------------------------------------------------------------------------
 PUBLIC bool keyboard_processing_main() {
-    uint8_t ch = 'm';   // TEMPORARY: flood test
+    uint32_t letter = 0;
     uint32_t size = 0; 
-    static uint8_t result = 0;
-    result = pio_spi_write8_blocking(&pio_collection, ((uint)ch<<24),&size, 1);
-  //  pio_sm_exec_wait_blocking(return_keyboard_miso_pio(),return_keyboard_miso_sm(),pio_encode_pull(false, true));
-  //  pio_sm_exec_wait_blocking(return_keyboard_miso_pio(),return_keyboard_miso_sm(),pio_encode_mov(pio_x, pio_osr));
+    uint32_t read = 0;
+    uint8_t ch = 'm';   // TEMPORARY: flood test
+    letter = ch << 24; //just to make sure it is at the MSB 
+    static uint32_t result = 0;
+    pio_spi_write8_blocking(&pio_collection, letter, &size);
+    read = read_register(&pio_collection,pio_x);
     static int gary_code_mismatch_count = 0;
     if(result == ch){
         gpio_put(PICO_CODE_DEBUG_PROBE_PIN,0);
@@ -361,16 +363,32 @@ PUBLIC bool keyboard_processing_main() {
         restore_interrupts_from_disabled(status);
         return(true);
     }
-    else if(size != GARY_CODE){
+    else{
         gary_code_mismatch_count++;
-        pio_sm_exec_wait_blocking(return_keyboard_miso_pio(),return_keyboard_miso_sm(),pio_encode_jmp(pio_collection.pio_miso.offset + spi_miso_loop_offset_keyboard_miso_entry)); // force PC back to keyboard_setup 
+        pio_sm_exec_wait_blocking(return_keyboard_miso_pio(), return_keyboard_miso_sm(), pio_encode_jmp(pio_collection.pio_miso.offset + spi_miso_loop_offset_keyboard_miso_entry)); // force PC back to keyboard_setup 
         uint32_t status = save_and_disable_interrupts();
         enqueue_interrupts(EVENT_NONE);
         restore_interrupts_from_disabled(status);
         return(false);
     }
-    // -------------------------------------------------------------
+
 }
+    // -------------------------------------------------------------
+
+PRIVATE uint32_t read_register(const pio_collection_t *pio_collection, const enum pio_src_dest reg) { // for debugging purposes.
+        uint move_isr = pio_encode_mov(pio_isr, reg);
+        pio_sm_exec_wait_blocking(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm, move_isr);
+        uint push = pio_encode_push(false, false);
+        pio_sm_exec_wait_blocking(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm, push);
+        return pio_sm_get(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm);
+    }
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+PRIVATE void __time_critical_func(pio_spi_write8_blocking)(const pio_collection_t *pio_collection, const uint32_t src, uint32_t *dest) {
+        uint debug = 0;
+        pio_sm_put_blocking(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm, src); // make sure the data is MSB first
+        *dest =  pio_sm_get_blocking(pio_collection->pio_mosi.pio, pio_collection->pio_mosi.sm); // read the data from the RX FIFO to clear /  debug = read_register(pio_collection, pio_osr); // read the OSR to see if the data is in the FIFO
+}
+
 
 ///PUBLIC bool keyboard_processing_main() {
 ///    uint8_t ch = 0;
@@ -410,6 +428,7 @@ PUBLIC bool keyboard_processing_main() {
 ///        return(false);
 ///}
 ///
+// -----------------------------------------------------------------------------
 PUBLIC void event_processing_main() {
     if(pio_interrupt_get(return_spi_pio(), 0)){
         pio_interrupt_clear(return_spi_pio(), 0);
@@ -423,102 +442,6 @@ PUBLIC void event_processing_main() {
     restore_interrupts_from_disabled(status);
 }
 
-PRIVATE uint read_register(const pio_collection_t *pio_collection, const enum pio_src_dest reg) { // for debugging purposes.
-    if(pio_interrupt_get(pio_collection->pio_miso.pio, 2)){
-        uint move_isr = pio_encode_mov(pio_isr, reg);
-        pio_sm_exec_wait_blocking(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm, move_isr);
-        uint push = pio_encode_push(false, false);
-        pio_sm_exec_wait_blocking(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm, push);
-        pio_interrupt_clear(pio_collection->pio_miso.pio, 2);
-    }
-    return pio_sm_get(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm);
-}
-
-// ------------------------------------------------------------------------------------------------------------------------------------------------------
-// FUNCTIONS TO READ AND WRITE TO PIO FIFOS
-// ------------------------------------------------------------------------------------------------------------------------------------------------------
-PRIVATE uint8_t __time_critical_func(pio_spi_write8_blocking)(const pio_collection_t *pio_collection, const uint8_t src, uint32_t *dest, size_t len) {
-    uint32_t debug = 0;
-  //  size_t tx_remain = len;
-  //  pio_sm_put(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm, (uint32_t)src<< 24); // make sure the data is MSB first
-
-      if (!pio_sm_is_tx_fifo_full(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm)) {
-          pio_sm_put(pio_collection->pio_miso.pio, pio_collection->pio_miso.sm, (uint32_t)src<<24); // make sure the data is MSB first
-          debug = read_register(pio_collection, pio_osr); // read the OSR to see if the data is in the FIFO
-
-      }
-        if (!pio_sm_is_rx_fifo_empty(pio_collection->pio_mosi.pio, pio_collection->pio_mosi.sm)) {
-            *dest = pio_sm_get_blocking(pio_collection->pio_mosi.pio, pio_collection->pio_mosi.sm); // read the data from the RX FIFO to clear it
-        }
-
-        debug = (debug>>24)&0xFF; // get the MSB of the debug value
-        return(debug);
-
-}
-
-
-///PRIVATE void __time_critical_func(pio_spi_write8_read8_blocking)(const pio_collection_t *pio_collection,const uint8_t *src, uint8_t *dst, size_t len) 
-///{
-///    size_t tx_remain = len;
-///    size_t rx_remain = len;
-///
-///    io_rw_8 *tx_fifo = (io_rw_8 *)&pio_collection->pio_miso.pio->txf[ pio_collection->pio_miso.sm ];
-///    io_ro_8 *rx_fifo = (io_ro_8 *)&pio_collection->pio_mosi.pio->rxf[ pio_collection->pio_mosi.sm ];
-///
-///    while (tx_remain || rx_remain) {
-///
-///        if (tx_remain && !pio_sm_is_tx_fifo_full( pio_collection->pio_miso.pio, pio_collection->pio_miso.sm))
-///        {
-///            *tx_fifo = *src++;
-///            --tx_remain;
-///        }
-///
-///        if (rx_remain && !pio_sm_is_rx_fifo_empty(pio_collection->pio_mosi.pio, pio_collection->pio_mosi.sm))
-///        {
-///            *dst++ = *rx_fifo;
-///            --rx_remain;
-///        }
-///    }
-///}
-
-// ----------------------------------------------------------------------------
-// CODE TO DEBUG PIO FROM WHAT I CAN GATHER 
-// ----------------------------------------------------------------------------
-
-///PRIVATE uint32_t ReadRxValue(PIO pio, uint sm) {
-///    uint32_t rx_value;
-///    if(sm == 0){
-///        rx_value = pio->rxf[0];
-///    } else if(sm == 1){
-///        rx_value = pio->rxf[1];
-///    } else if(sm == 2){
-///        rx_value = pio->rxf[2];
-///    } else if(sm == 3){
-///        rx_value = pio->rxf[3];
-///    }
-///    return rx_value;
-///}
-///
-///PRIVATE uint32_t ReadTxValue(PIO pio, uint sm) {
-///    uint32_t tx_value;
-///    if(sm == 0){
-///        tx_value = pio->txf[0];
-///    } else if(sm == 1){
-///        tx_value = pio->txf[1];
-///    } else if(sm == 2){
-///        tx_value = pio->txf[2];
-///    } else if(sm == 3){
-///        tx_value = pio->txf[3];
-///    }
-///    return tx_value;
-///}
-///}
-
-
-// -----------------------------------------------------------------------------
-
-// Probably dont need this function, but just in case.
-   
 
 
                
